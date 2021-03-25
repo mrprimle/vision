e2944d5bf7cfe45f6aab67802c0fac5ebc7e6c98
Mike D <30389608+mrprimle@users.noreply.github.com>
2021-03-25 23:59:34 +0200

    accord test

--- js.js
+++ js.js
@@ -105,6 +105,9 @@ Scroller.prototype = {
           $(".founder .founder-text").addClass("slideH-right");
           $(".founder .founder-wrapImg").addClass("slideH-right");
       }
+      if (currentScrollY > $(".faq .section-header").offset().top - wHeight / 1.3) {
+          $(".faq .section-header").addClass("slideH-right");
+      }
       
   }
 };
@@ -112,6 +115,46 @@ Scroller.prototype = {
 
 var scroller = new Scroller();  
 scroller.init();
+   
+    
+    
+    let parent = document.querySelectorAll("[data-collapse");
+			parent.forEach((element) => {
+				let clickTarget = element.querySelector("*");
+				let collapseElement = element.nextElementSibling;
+				let collapseElementChildren = collapseElement.children;
+				let tl = new TimelineMax({
+					reversed: true,
+					paused: true,
+				});
+
+				tl.from(
+					collapseElement,
+					1.6,
+					{
+						className: "+=heightZero",
+						ease: Expo.easeInOut,
+					},
+					"open"
+				);
+				tl.staggerFrom(
+					collapseElementChildren,
+					1,
+					{
+						autoAlpha: 0,
+						y: "40%",
+						ease: Expo.easeInOut,
+					},
+					0.08,
+					"open+=.1"
+				);
+
+				clickTarget.addEventListener("click", () => {
+					tl.reversed() ? tl.play() : tl.reverse();
+				});
+			});
+    
+    
     
 var isOpenMenu = false;    
 $(".menu-icon").on("click", function() {
